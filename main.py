8a459fa305e565a9891aa3d16a23418002316cf5
mahabubalam-gbs <mahabubalam.gbs@gmail.com>
2025-12-10 00:41:02 +0600

    Square Pattern & Increasing Triangle Pattern

---
+++ main.py
@@ -0,0 +1,14 @@
+# Square Pattern
+n = 5
+for i in range(n):
+    for j in range(n):
+        print("*", end=" ")
+    print()
+
+# Increasing Triangle Pattern
+n = 5
+for i in range(n):
+    for j in range(i+1) :
+        print("*", end=" ")
+    print()
+
