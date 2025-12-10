5c18bda5611ff6f21991e92fc7b5a42bd588de24
mahabubalam-gbs <mahabubalam.gbs@gmail.com>
2025-12-11 01:00:00 +0600

    Right Sided Triangle Pattern 1

--- main.py
+++ main.py
@@ -13,8 +13,18 @@ for i in range(n):
     print()
 
 # Decreasing Triangle Pattern
-n =5
+n = 5
 for i in range(n):
     for j in range(i, 5):
         print("*", end=" ")
     print()
+
+# Right Sided Triangle
+n = 5
+for i in range(n):
+    for j in range(i, 5):
+        print("", end=" ")
+    for k in range(i+1):
+        print("*", end=" ")
+    print()
+
