8349b19178f56f06cc3df8b7fc182aeb295c340e
mahabubalam-gbs <mahabubalam.gbs@gmail.com>
2025-12-11 01:44:03 +0600

    Diamond Pattern

--- main.py
+++ main.py
@@ -58,3 +58,22 @@ for i in range(n):
     for l in range(i, n):
         print("*", end=" ")
     print()
+
+# Diamond Pattern
+n = 5
+for i in range(n-1):
+    for j in range(i, n):
+        print(" ", end=" ")
+    for k in range(i):
+        print("*", end=" ")
+    for l in range(i+1):
+        print("*", end=" ")
+    print()
+for i in range(n):
+    for j in range(i+1):
+        print(" ", end=" ")
+    for k in range(i, n-1):
+        print("*", end=" ")
+    for l in range(i, n):
+        print("*", end=" ")
+    print()
