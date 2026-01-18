e54f987a90f0cb5186177d025f716611a8c4cb1d
mahabubalam-gbs <mahabubalam.gbs@gmail.com>
2026-01-18 22:10:00 +0600

    Add Increasing Triangle Number:1's Pattern

--- Problem_Solving.py
+++ Problem_Solving.py
@@ -165,3 +165,9 @@ for i in range(n):
         print("*", end=" ")
     print()
 
+# Increasing Triangle (Number: 1's) Pattern
+n = 5
+for i in range(n):
+    for j in range(i+1):
+        print('1', end=" ")
+    print()
