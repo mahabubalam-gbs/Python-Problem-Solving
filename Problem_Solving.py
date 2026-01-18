3ef2dd1dfb1d4af42ead08090ce29d23dd930b59
mahabubalam-gbs <mahabubalam.gbs@gmail.com>
2026-01-18 21:50:33 +0600

    Increasing Triangle (Number: 1's)

---
+++ Problem_Solving.py
@@ -0,0 +1,86 @@
+# Square Pattern
+n = 5
+for i in range(n):
+    for j in range(n):
+        print("*", end=" ")
+    print()
+
+# Increasing Triangle Pattern
+n = 5
+for i in range(n):
+    for j in range(i+1) :
+        print("*", end=" ")
+    print()
+
+# Decreasing Triangle Pattern
+n = 5
+for i in range(n):
+    for j in range(i, 5):
+        print("*", end=" ")
+    print()
+
+# Right Sided Triangle Pattern 1
+n = 5
+for i in range(n):
+    for j in range(i, 5):
+        print(" ", end=" ")
+    for k in range(i+1):
+        print("*", end=" ")
+    print()
+
+# Right Sided Triangle Pattern 2
+n = 5
+for i in range(n):
+    for j in range(i+1):
+        print(" ", end=" ")
+    for k in range(i, n):
+        print("*", end=" ")
+    print()
+
+# Hill Pattern
+n = 5
+for i in range(n):
+    for j in range(i, n):
+        print(" ", end=" ")
+    for k in range(i):
+        print("*", end=" ")
+    for l in range(i+1):
+        print("*", end=" ")
+    print()
+
+# Reverse Hill Pattern
+n = 5
+for i in range(n):
+    for j in range(i+1):
+        print(" ", end=" ")
+    for k in range(i, n-1):
+        print("*", end=" ")
+    for l in range(i, n):
+        print("*", end=" ")
+    print()
+
+# Diamond Pattern
+n = 5
+for i in range(n-1):
+    for j in range(i, n):
+        print(" ", end=" ")
+    for k in range(i):
+        print("*", end=" ")
+    for l in range(i+1):
+        print("*", end=" ")
+    print()
+for i in range(n):
+    for j in range(i+1):
+        print(" ", end=" ")
+    for k in range(i, n-1):
+        print("*", end=" ")
+    for l in range(i, n):
+        print("*", end=" ")
+    print()
+
+# Increasing Triangle (Number: 1's)
+n = 5
+for i in range(n):
+    for j in range(i+1):
+        print("1", end=" ")
+    print()
