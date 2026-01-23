2a597182f58f92488603ed6c50446549e6c25a5c
mahabubalam-gbs <mahabubalam.gbs@gmail.com>
2026-01-23 23:05:32 +0600

    Incrementing Rows Triangle

--- Problem_Solving.py
+++ Problem_Solving.py
@@ -171,3 +171,10 @@ for i in range(n):
     for j in range(i+1):
         print('1', end=" ")
     print()
+
+# Incrementing Rows Triangle
+n =5
+for i in range(n):
+    for j in range(i +1):
+        print(i +1, end=" ")
+    print()
